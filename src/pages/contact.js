0130a76b68a7bb0a6b95e1b7ec7a3ced8bc10457
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-06 15:40:25 -0800

    add links to about and contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact Me!</h1>
-            <p>Follow me on Twitter and Instagram: @chaselancaster_</p>
+            <p>Follow me on Twitter <a href='https://twitter.com/chaselancaster_' target='_blank'>here!</a></p>
         </div>
     )
 }
