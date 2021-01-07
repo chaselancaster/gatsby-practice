980d24d6bb83bd116b83f0ed3a549254ca5cf643
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-06 17:08:47 -0800

    add content in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,23 @@ import { Link} from 'gatsby'
 const Header = () => {
     return (
         <div>
-            
+            <h1>Chase Lancaster</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/blog'>Blog</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
